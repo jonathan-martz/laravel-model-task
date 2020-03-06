d35c04d8f6038e158f865ba157ec10738e45ea97
jonathan-martz <github@jmartz.de>
2020-03-06 21:43:40 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+#### Task Model for Laravel 
+Note: This Model is in development, dont use it in production !!!
+
+#### Why ?
+This Model is to Handle Task Data for my Lumen and Nova Projects.
+
+#### Todo
+* move migrations into own module ?
+
+##### Copyright
+Copyright by Jonathan Martz
---
+++ model/Project.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Model;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
+
+/**
+ * Class User
+ * @package App\Model
+ */
+class Task extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'id', 'name', 'description', 'PID', 'PTID'
+    ];
+
+    /**
+     * The attributes excluded from the model's JSON form.
+     *
+     * @var array
+     */
+    protected $hidden = [];
+
+    /**
+     * User constructor.
+     * @param array $attributes
+     */
+    public function __construct($attributes = [])
+    {
+        if(!is_array($attributes)) {
+            $attributes = (array)$attributes;
+        }
+        parent::__construct($attributes);
+    }
+}
